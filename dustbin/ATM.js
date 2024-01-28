72ba7452af16cab14c5d7ec8318ba9de552cd3a0
Muhammad Javaid <jvdbms2131@gmail.com>
2024-01-28 09:42:17 +0500

    update

---
+++ dustbin/ATM.js
@@ -0,0 +1,57 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "input",
+        name: "UserID",
+        message: "Kindly enter your UserID",
+    },
+    {
+        type: "input",
+        name: "UserPIN",
+        message: "Kindly enter your UserPIN",
+    },
+    {
+        type: "list",
+        name: "TransactionType",
+        choices: ["Fast Cash", "Withdraw"],
+        message: "Kindly enter your TransactionType",
+        when(answers) {
+            return answers.accountType;
+        }
+    },
+    {
+        type: "list",
+        name: "AcountType",
+        choices: ["Saving", "Current"],
+        message: "Kindly enter your AccountType",
+    },
+    {
+        type: "list",
+        name: "Amount",
+        choices: [1000, 2000, 10000, 20000],
+        message: "Kindly enter your Amount",
+        when(answers) {
+            return answers.TransactionType == 'Fast Cash';
+        },
+    },
+    {
+        type: "list",
+        name: "Amount",
+        message: "Kindly enter your Amount",
+        when(answers) {
+            return answers.TransactionType == "Withdraw";
+        },
+    },
+]);
+if (answers.userID && answers.userPIN) {
+    const balance = Math.floor(Math.random() * 10000000);
+    console.log(balance);
+    const EnteredAmount = answers.Amount;
+    if (balance > EnteredAmount) {
+        const remaining = balance - EnteredAmount;
+        console.log("Your Remaining Balance is:", remaining);
+    }
+    else {
+        console.log("Insufficient Balance");
+    }
+}
