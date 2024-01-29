c58d6945fc5f9b88d46a675d96a6900d46e15799
Muhammad Javaid <jvdbms2131@gmail.com>
2024-01-29 17:07:56 +0500

    update

--- ATM.ts
+++ ATM.ts
@@ -22,10 +22,7 @@ const answers1: ansType = await inquirer.prompt([
         name: "TransactionType",
         choices: ["Fast Cash", "Withdraw"],
         message: "Kindly enter your TransactionType",
-        when (answers) {
-            return answers.accountType
-        }
-      },
+           },
       {
         type: "list",
         name: "AcountType",
@@ -37,19 +34,13 @@ const answers1: ansType = await inquirer.prompt([
         name: "Amount",
         choices: [1000,2000,10000,20000],
         message: "Kindly enter your Amount",
-        when (answers) {
-            return answers.TransactionType == 'Fast Cash'
-        },
-      
+             
     },
     {
         type: "list",
         name: "Amount",
         message: "Kindly enter your Amount",
-        when (answers) {
-            return answers.TransactionType == "Withdraw"
-        },
-      
+ 
     },
 
 ]) 
@@ -71,68 +62,68 @@ if(answers1.userID && answers1.userPIN) {
 
 
 
-interface ansType{
-    userID: string,
-    userPIN: number,
-    TransactionType: string,
-    AcountType: string,
-    Amount: number
-}
-const answers : ansType = await inquirer.prompt([
-    {
-        type: "input",
-        name: "UserID",
-        choices: ["Abeera","Abeera Javaid", "Abeera  Ramla Javaid"],
-        message: "Kindly enter your UserID",
-    },
-    {
-        type: "input",
-        name: "UserPIN",
-        choices: [7 , 1 , 2012],
-        message: "Kindly select your UserPIN",
-    },
-    {
-      type: "list",
-      name: "TransactionType",
-      choices: ["Fast Cash", "Withdraw"],
-      message: "Kindly selecy  your TransactionType",
-      when (answers) {
-          return answers.accountType
-      }
-    },
-    {
-        type: "list",
-        name: "AcountType",
-        choices: ["Saving", "Current"],
-        message: "Kindly select your AccountType",
-    },
-    {
-        type: "list",
-        name: "Amount",
-        choices: [1000000,20000000,300000000,4000000000,500000000,600000000,700000000,800000000,900000000,1000000000],
-        message: "Kindly select your Amount",
-        when (answers) {
-            return answers.TransactionType == 'Fast Cash'
-        },
-    },
-    {
-      type: "list",
-      name: "Amount",
-      message: "Kindly enter your Amount",
-      when (answers) {
-          return answers.TransactionType == "Withdraw"
-      },
-    }
-])
-if(answers.userID && answers.userPIN) {
-const balance = Math.floor(Math.random() *1000000000);
-console.log(balance);
-const EnteredAmount = answers.Amount;
-if(balance > EnteredAmount) {
-    const remaining = balance - EnteredAmount;
-    console.log("Your Remaining Balance is:",  remaining);
+// interface ansType{
+//     userID: string,
+//     userPIN: number,
+//     TransactionType: string,
+//     AcountType: string,
+//     Amount: number
+// }
+// const answers : ansType = await inquirer.prompt([
+//     {
+//         type: "input",
+//         name: "UserID",
+//         choices: ["Abeera","Abeera Javaid", "Abeera  Ramla Javaid"],
+//         message: "Kindly enter your UserID",
+//     },
+//     {
+//         type: "input",
+//         name: "UserPIN",
+//         choices: [7 , 1 , 2012],
+//         message: "Kindly select your UserPIN",
+//     },
+//     {
+//       type: "list",
+//       name: "TransactionType",
+//       choices: ["Fast Cash", "Withdraw"],
+//       message: "Kindly selecy  your TransactionType",
+//       when (answers) {
+//           return answers.accountType
+//       }
+//     },
+//     {
+//         type: "list",
+//         name: "AcountType",
+//         choices: ["Saving", "Current"],
+//         message: "Kindly select your AccountType",
+//     },
+//     {
+//         type: "list",
+//         name: "Amount",
+//         choices: [1000000,20000000,300000000,4000000000,500000000,600000000,700000000,800000000,900000000,1000000000],
+//         message: "Kindly select your Amount",
+//         when (answers) {
+//             return answers.TransactionType == 'Fast Cash'
+//         },
+//     },
+//     {
+//       type: "list",
+//       name: "Amount",
+//       message: "Kindly enter your Amount",
+//       when (answers) {
+//           return answers.TransactionType == "Withdraw"
+//       },
+//     }
+// ])
+// if(answers.userID && answers.userPIN) {
+// const balance = Math.floor(Math.random() *1000000000);
+// console.log(balance);
+// const EnteredAmount = answers.Amount;
+// if(balance > EnteredAmount) {
+//     const remaining = balance - EnteredAmount;
+//     console.log("Your Remaining Balance is:",  remaining);
     
-}else{
-    console.log("Insufficient Balance");
-}
-};
+// }else{
+//     console.log("Insufficient Balance");
+// }
+// };
